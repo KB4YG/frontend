3232bf668dcc8aca0605384f8a0b70af6c67515b
clemoss <christopher.lemoss@gmail.com>
2022-01-07 20:46:11 -0800

    Updated parking info

--- lib/screens/parking_info.dart
+++ lib/screens/parking_info.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kb4yg/utilities/screen_arguments.dart';
 import 'package:kb4yg/utilities/constants.dart' as constants;
+import 'package:kb4yg/widgets/settings.dart';
 
 
 class ParkingInfo extends StatefulWidget {
@@ -15,6 +16,24 @@ class _ParkingInfoState extends State<ParkingInfo> {
   static const tableTitle = 'Parking Information';
   Map data = {};
 
+  void pushSelectCounty () async {
+    // Prompt user with SelectCounty() screen
+    dynamic result = await Navigator.pushNamed(
+        context,
+        constants.navSelectCounty,
+        arguments: ScreenArguments(
+            county: data[argCounty],
+            locations: data[argLocations]
+        )
+    );
+    // Update state with new county (if user selected one)
+    if (result != null) {
+      setState(() {
+        data = {argCounty: result.county, argLocations: result.locations};
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -23,36 +42,25 @@ class _ParkingInfoState extends State<ParkingInfo> {
 
     // Update data if first time running
     if (data.isEmpty) {
-      final args = ModalRoute.of(context)?.settings.arguments as ScreenArguments;
-      data = {argCounty: args.county, argLocations: args.locations};
+      final settings = ModalRoute.of(context)?.settings;
+      if (settings?.arguments != null) {
+        final args = settings!.arguments as ScreenArguments;
+        data = {argCounty: args.county, argLocations: args.locations};
+      }
     }
 
     return Scaffold(
+      endDrawer: const Settings(),
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        // backgroundColor: Colors.green,
         centerTitle: true,
         elevation: 0,
-        title: TextButton(
-          child: Text(
+        title: TextButton.icon(
+          icon: const Icon(Icons.edit_location),
+          label: Text(
             '${data[argCounty]} County',
-            style: const TextStyle(color: Colors.white, fontSize: 20),),
-          onPressed: () async {
-            // Prompt user with SelectCounty() screen
-            dynamic result = await Navigator.pushNamed(
-                context,
-                constants.navSelectCounty,
-                arguments: ScreenArguments(
-                    county: data[argCounty],
-                    locations: data[argLocations]
-                )
-            );
-            // Update state with new county (if user selected one)
-            if (result != null) {
-              setState(() {
-                data = {argCounty: result.county, argLocations: result.locations};
-              });
-            }
-          }
+            style: const TextStyle(color: Colors.white, fontSize: 20)),
+          onPressed: pushSelectCounty,
         )
       ),
       body: SingleChildScrollView(
@@ -63,8 +71,9 @@ class _ParkingInfoState extends State<ParkingInfo> {
               // TODO: add styling
               const Text(tableTitle),
               // Parking information table
+              // TODO: turn into widget
               Container(
-                color: Colors.white,
+                // color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 20, horizontal: width * 0.05),
                 constraints: const BoxConstraints(minWidth: 200, maxWidth: 600),
                 child: DataTable(
@@ -100,6 +109,7 @@ class _ParkingInfoState extends State<ParkingInfo> {
                     for (var loc in data[argLocations]) DataRow(
                       onSelectChanged: (bool? selected) {
                         if (selected == true) {
+                          // TODO: add screen for location
                           print(loc.name);
                         }},
                       cells: [
