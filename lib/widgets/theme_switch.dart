5d4acee316c61e70f1ea3f9a2b3ba0c37ef01c37
clemoss <christopher.lemoss@gmail.com>
2022-01-08 16:06:43 -0800

    Added light and dark word to theme switch

--- lib/widgets/theme_switch.dart
+++ lib/widgets/theme_switch.dart
@@ -2,18 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:kb4yg/providers/theme.dart';
 
-class ThemeSwitch extends StatelessWidget {
+class ThemeSwitch extends StatefulWidget {
   const ThemeSwitch({Key? key}) : super(key: key);
+
+  @override
+  State<ThemeSwitch> createState() => _ThemeSwitchState();
+}
+
+class _ThemeSwitchState extends State<ThemeSwitch> {
+  static bool _isDark = false;
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return SwitchListTile(
       visualDensity: VisualDensity.compact,
-      title: const Text('Dark Mode'),
+      title: Text('${_isDark ? 'Dark' : 'Light'} Mode'),
       secondary: const Icon(Icons.lightbulb_outline),
       value: themeProvider.isDark,
       onChanged: (value) {
+        setState(() {
+          _isDark = value;
+        });
         final provider = Provider.of<ThemeProvider>(context, listen: false);
         provider.toggleTheme(value);
       }
