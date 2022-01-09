92019964d3e0ca12765be133d64a2a5011af1435
clemoss <christopher.lemoss@gmail.com>
2022-01-08 18:04:50 -0800

    Fixed bug where name of theme mode was incorrect on page reload

--- lib/widgets/theme_switch.dart
+++ lib/widgets/theme_switch.dart
@@ -2,28 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:kb4yg/providers/theme.dart';
 
-class ThemeSwitch extends StatefulWidget {
+class ThemeSwitch extends StatelessWidget {
   const ThemeSwitch({Key? key}) : super(key: key);
 
-  @override
-  State<ThemeSwitch> createState() => _ThemeSwitchState();
-}
-
-class _ThemeSwitchState extends State<ThemeSwitch> {
-  static bool _isDark = false;
+  bool isDark(context) => Theme.of(context).brightness  == Brightness.dark;
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return SwitchListTile(
       visualDensity: VisualDensity.compact,
-      title: Text('${_isDark ? 'Dark' : 'Light'} Mode'),
+      title: Text('${isDark(context) ? 'Dark' : 'Light'} Mode'),
       secondary: const Icon(Icons.lightbulb_outline),
       value: themeProvider.isDark,
       onChanged: (value) {
-        setState(() {
-          _isDark = value;
-        });
         final provider = Provider.of<ThemeProvider>(context, listen: false);
         provider.toggleTheme(value);
       }
