3783bc8b63172f3dc61aaa6f9fc7a44f084dd654
clemoss <christopher.lemoss@gmail.com>
2022-01-16 15:34:53 -0800

    Formatting

--- lib/widgets/theme_switch.dart
+++ lib/widgets/theme_switch.dart
@@ -5,20 +5,16 @@ import 'package:kb4yg/providers/theme.dart';
 class ThemeSwitch extends StatelessWidget {
   const ThemeSwitch({Key? key}) : super(key: key);
 
-  bool isDark(context) => Theme.of(context).brightness  == Brightness.dark;
+  bool isDark(context) => Theme.of(context).brightness == Brightness.dark;
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return SwitchListTile(
-      visualDensity: VisualDensity.compact,
-      title: Text('${isDark(context) ? 'Dark' : 'Light'} Mode'),
-      secondary: const Icon(Icons.lightbulb_outline),
-      value: themeProvider.isDark,
-      onChanged: (value) {
-        final provider = Provider.of<ThemeProvider>(context, listen: false);
-        provider.toggleTheme(value);
-      }
-    );
+        visualDensity: VisualDensity.compact,
+        title: Text('${isDark(context) ? 'Dark' : 'Light'} Mode'),
+        secondary: const Icon(Icons.lightbulb_outline),
+        value: themeProvider.isDark,
+        onChanged: themeProvider.toggleTheme);
   }
 }
