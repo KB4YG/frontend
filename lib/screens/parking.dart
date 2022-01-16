3c3882566c5974aa3ced1b936953006e1d89bda1
clemoss <christopher.lemoss@gmail.com>
2022-01-16 15:29:47 -0800

    Fixed routing errors and moved data table into /widgets/parking_table.dart

--- lib/screens/parking.dart
+++ lib/screens/parking.dart
@@ -1,135 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:kb4yg/county.dart';
+import 'package:kb4yg/widgets/parking_table.dart';
 import 'package:kb4yg/widgets/settings.dart';
 import 'package:kb4yg/widgets/fire_safety.dart';
 import 'package:kb4yg/utilities/screen_arguments.dart';
 import 'package:kb4yg/utilities/constants.dart' as constants;
 
-class ParkingInfo extends StatefulWidget {
-  const ParkingInfo({Key? key}) : super(key: key);
+class Parking extends StatelessWidget {
+  final County county;
+  const Parking({Key? key, required this.county}) : super(key: key);
 
-  @override
-  State<ParkingInfo> createState() => _ParkingInfoState();
-}
-
-class _ParkingInfoState extends State<ParkingInfo> {
-
-  static const tableTitle = 'Parking Information';
-  Map data = {};
-
-  void pushSelectCounty () async {
+  void pushSelectCounty (BuildContext context) {
     // Prompt user with SelectCounty() screen
-    dynamic result = await Navigator.pushNamed(
-        context,
-        constants.navSelectCounty,
-        arguments: ScreenArguments(county: data[argCounty])
+    Navigator.pushNamed(
+      context,
+      constants.routeParking,
+      arguments: ScreenArguments(county: county)
     );
-    // Update state with new county (if user selected one)
-    if (result != null) {
-      setState(() {
-        data = {argCounty: result.county};
-      });
-    }
   }
 
   @override
   Widget build(BuildContext context) {
-
-    // Get screen width for table margin percentage
-    final double width = MediaQuery.of(context).size.width;
-
-    // Update data if first time running
-    if (data.isEmpty) {
-      final settings = ModalRoute.of(context)?.settings;
-      if (settings?.arguments != null) {
-        final args = settings!.arguments as ScreenArguments;
-        data = {argCounty: args.county};
-      }
-    }
-
     return Scaffold(
       endDrawer: const Settings(),
       appBar: AppBar(
-        // backgroundColor: Colors.green,
         centerTitle: true,
         elevation: 0,
         title: TextButton.icon(
           icon: const Icon(Icons.edit_location),
-          label: Text(
-            '${data[argCounty].name} County',
+          label: Text('$county County',
             style: const TextStyle(color: Colors.white, fontSize: 20)),
-          onPressed: pushSelectCounty,
+          onPressed: () {
+            pushSelectCounty(context);
+          },
         )
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             children: [
-              // Parking table title
-              // TODO: add styling
-              const Text(tableTitle),
-              // Parking information table
-              // TODO: turn into widget
-              Container(
-                // color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 20, horizontal: width * 0.05),
-                constraints: const BoxConstraints(minWidth: 200, maxWidth: 600),
-                child: DataTable(
-                  // TODO: add ability to sort columns
-                  columnSpacing: 45,
-                  showCheckboxColumn: false,
-                  columns: const [
-                    DataColumn(
-                      tooltip: 'Name of recreation area',
-                      label: Expanded(
-                        child: Text('Location',
-                          textAlign: TextAlign.center,
-                          textScaleFactor: 1.5,
-                          style: TextStyle(
-                            //TODO
-                          ),
-                        ),
-                      )
-                    ),
-                    DataColumn(
-                      numeric: true,
-                      tooltip: 'General parking spots currently available',
-                      label: Icon(Icons.directions_car)
-                    ),
-                    DataColumn(
-                      numeric: true,
-                      tooltip: 'Handicap parking spots currently available',
-                      label: Icon(Icons.accessible)
-                    )
-                  ],
-                  rows: [
-                    // Parking Information
-                    for (var loc in data[argCounty].locs) DataRow(
-                      onSelectChanged: (bool? selected) {
-                        if (selected == true) {
-                          // TODO: add screen for location
-                          print(loc.name);
-                        }},
-                      cells: [
-                        DataCell(Text(
-                          loc.name,
-                          textScaleFactor: 1.25
-                          // style: TextStyle(fontSize: 25),
-                        )),
-                        DataCell(Center(
-                          child: Text(
-                              loc.spots.toString(),
-                              textAlign: TextAlign.center),
-                        )),
-                        DataCell(Center(
-                          child: Text(
-                            loc.handicap.toString(),
-                            textAlign: TextAlign.center,
-                          ),
-                        )),
-                    ]),
-                  ]),
-              ),
-              FireSafety(county: data[argCounty])
+              // const Text('Parking Information'), // TODO: add styling
+              ParkingTable(county: county),
+              FireSafety(county: county)
           ]),
         ),
       ),
