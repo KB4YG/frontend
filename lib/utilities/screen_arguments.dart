45ae96ad3bf3153ae6cd08322a837168e50145d9
clemoss <christopher.lemoss@gmail.com>
2022-01-17 13:09:10 -0800

    Added navigator index and location arguments

--- lib/utilities/screen_arguments.dart
+++ lib/utilities/screen_arguments.dart
@@ -1,11 +1,18 @@
-import 'package:kb4yg/county.dart';
+import 'package:flutter/material.dart' show RouteSettings;
+import 'package:kb4yg/models/access_point.dart';
+import 'package:kb4yg/models/county.dart';
 
 const argCounty = 0;
 
 class ScreenArguments {
   final County? county;
+  final AccessPoint? location;
+  final int? navIndex;
 
-  ScreenArguments({this.county});
+  ScreenArguments({this.county, this.location, this.navIndex});
+
+  static getArgs(RouteSettings settings) =>
+      settings.arguments == null ? null : settings.arguments as ScreenArguments;
 
   @override
   String toString() => 'ScreenArguments($county)';
