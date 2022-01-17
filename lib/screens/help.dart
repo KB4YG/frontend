661487c2abb18cf6aec3d40e41baa4a4f7957d47
clemoss <christopher.lemoss@gmail.com>
2022-01-17 13:04:11 -0800

    Changed to stateless widget

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:kb4yg/widgets/buttomNavBar.dart';
+import 'package:kb4yg/widgets/bottom_nav_bar.dart';
 import 'package:kb4yg/widgets/settings.dart';
 
-class Help extends StatefulWidget {
-  const Help({Key? key, required this.loc_name, required this.navBarIndex})
-      : super(key: key);
-  final String loc_name;
-  final int navBarIndex;
-  @override
-  _HelpState createState() => _HelpState();
-}
+class Help extends StatelessWidget {
+  const Help({Key? key}) : super(key: key);
 
-class _HelpState extends State<Help> {
-  String screenName = 'Help';
-  //static int navBarIndex = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,17 +13,15 @@ class _HelpState extends State<Help> {
         automaticallyImplyLeading: true,
         centerTitle: true,
         elevation: 0,
-        title: Text(
-          screenName,
-          style: const TextStyle(color: Colors.white, fontSize: 20),
+        title: const Text('Help',
+          style: TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
       endDrawer: const Settings(),
       body: const Center(
         child: Text('data'),
       ),
-      bottomNavigationBar:
-          BottonNavBar(navBarIndex: widget.navBarIndex, loc_name: widget.loc_name),
+      bottomNavigationBar: const BottomNavBar(navIndex: 2)
     );
   }
 }
