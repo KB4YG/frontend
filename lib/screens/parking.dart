7b4b4f07ebc4cada2dd59998a726b9b91639c47e
clemoss <christopher.lemoss@gmail.com>
2022-01-27 18:38:32 -0800

    Added beamer

--- lib/screens/parking.dart
+++ lib/screens/parking.dart
@@ -1,3 +1,4 @@
+import 'package:beamer/beamer.dart' show Beamer;
 import 'package:flutter/material.dart';
 import 'package:kb4yg/models/county.dart';
 import 'package:kb4yg/screens/help.dart';
@@ -11,68 +12,29 @@ import 'package:kb4yg/utilities/constants.dart' as constants;
 
 import 'about.dart';
 
-class Parking extends StatelessWidget {
+class CountyDetailsScreen extends StatelessWidget {
   final County county;
-  const Parking({Key? key, required this.county}) : super(key: key);
-//
-//   @override
-//   State<Parking> createState() => _ParkingState();
-// }
-//
-// class _ParkingState extends State<Parking> {
-//   int _selectedIndex = 0;
-//   get county => widget.county;
-//   late final List<Widget> _screens;
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _screens = [
-  //     SingleChildScrollView(
-  //       child: Center(
-  //         child: Column(
-  //             children: [
-  //               // const Text('Parking Information'), // TODO: add styling
-  //               ParkingTable(county: county),
-  //               FireSafety(county: county)
-  //             ]),
-  //       ),
-  //     ),
-  //     const About(navBarIndex: 0),
-  //     const Help(loc_name: '', navBarIndex: 0),
-  //   ];
-  // }
-
-  // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //   });
-  // }
-
-  void pushSelectCounty(BuildContext context) {
-    // Prompt user with SelectCounty() screen
-    Navigator.pushNamed(context, constants.routeParking,
-        arguments: ScreenArguments(county: county));
-  }
+  const CountyDetailsScreen({Key? key, required this.county}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      endDrawer: const Settings(),
       appBar: Header(
           title: TextButton.icon(
             icon: const Icon(Icons.edit_location),
-            label: Text('$county County',
+            label: Text('${county.name} County',
                 style: const TextStyle(color: Colors.white, fontSize: 20)),
             onPressed: () {
-              pushSelectCounty(context);
+              Beamer.of(context).beamToNamed(
+                  constants.routeLocations,
+                  data: ScreenArguments(county: county)
+              );
             },
           )),
-      bottomNavigationBar: const BottomNavBar(navIndex: 0),
+      endDrawer: const Settings(),
       body: SingleChildScrollView(
         child: Center(
           child: Column(children: [
-            // const Text('Parking Information'), // TODO: add styling
             ParkingTable(county: county),
             FireSafety(county: county)
           ]),
