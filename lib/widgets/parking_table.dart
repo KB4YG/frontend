4c0de7fff4e83bf550da2f5e23dc4c7656040226
clemoss <christopher.lemoss@gmail.com>
2022-01-27 22:30:02 -0800

    Added sorting

--- lib/widgets/parking_table.dart
+++ lib/widgets/parking_table.dart
@@ -1,58 +1,75 @@
+import 'package:beamer/beamer.dart' show Beamer;
 import 'package:flutter/material.dart';
 import 'package:kb4yg/models/county.dart';
-import 'package:kb4yg/screens/parking_area.dart' show pushLocationScreen;
+import 'package:kb4yg/utilities/constants.dart' as constants;
+import 'package:kb4yg/utilities/sanitize_url.dart';
 
-class ParkingTable extends StatelessWidget {
+
+class ParkingTable extends StatefulWidget {
   final County county;
   const ParkingTable({Key? key, required this.county}) : super(key: key);
 
   @override
+  State<ParkingTable> createState() => _ParkingTableState();
+}
+
+class _ParkingTableState extends State<ParkingTable> {
+  int _columnIndex = 0;
+  bool _isAscending = true;
+
+  @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
     return Container(
-      // color: Colors.white,
-      margin: EdgeInsets.symmetric(vertical: 20, horizontal: width * 0.05),
+      margin: const EdgeInsets.all(20),
       constraints: const BoxConstraints(minWidth: 200, maxWidth: 600),
       child: DataTable(
-        // TODO: add ability to sort columns
-        columnSpacing: 45,
+        sortColumnIndex: _columnIndex,
+        sortAscending: _isAscending,
         showCheckboxColumn: false,
-        columns: const [
+        columns: [
           DataColumn(
-              tooltip: 'Name of recreation area',
-              label: Expanded(
-                child: Text(
-                  'Location',
-                  textAlign: TextAlign.center,
-                  textScaleFactor: 1.5,
-                  style: TextStyle(
-                      //TODO
-                      ),
-                ),
-              )),
+            onSort: onSort,
+            tooltip: 'Name of recreation area',
+            label: const Expanded(
+              child: Text(
+                'Location',
+                textAlign: TextAlign.center,
+                textScaleFactor: 1.4,
+                // TODO: text style
+              ),
+            )),
           DataColumn(
-              numeric: true,
-              tooltip: 'General parking spots currently available',
-              label: Icon(Icons.directions_car)),
+            onSort: onSort,
+            numeric: true,
+            tooltip: 'General parking spots currently available',
+            label: const Padding(
+              padding: EdgeInsets.only(right: 14.0),
+              child: Icon(Icons.directions_car),
+            )),
           DataColumn(
-              numeric: true,
-              tooltip: 'Handicap parking spots currently available',
-              label: Icon(Icons.accessible))
+            onSort: onSort,
+            numeric: true,
+            tooltip: 'Handicap-accessible parking spots currently available',
+            label: const Padding(
+              padding: EdgeInsets.only(right: 14.0),
+              child: Icon(Icons.accessible),
+            ))
         ],
         rows: [
-          for (var loc in county.locs)
+          for (var loc in widget.county.locs)
             DataRow(
               onSelectChanged: (bool? selected) {
                 if (selected == true) {
-                  // TODO: add screen for location
-                  print(loc.name);
-                  pushLocationScreen(context, loc);
+                  String route = constants.routeLocations;
+                  route += sanitizeUrl('/${widget.county.name}/${loc.name}');
+                  Beamer.of(context).beamToNamed(route);
                 }
               },
               cells: [
                 DataCell(Text(loc.name, textScaleFactor: 1.25)),
                 DataCell(Center(
-                  child: Text(loc.spots.toString(),
+                  child: Text(
+                    loc.spots.toString(),
                     textAlign: TextAlign.center),
                 )),
                 DataCell(Center(
@@ -64,5 +81,29 @@ class ParkingTable extends StatelessWidget {
               ]),
         ]),
     );
+  }
+
+  void onSort(int columnIndex, bool isAscending) {
+    switch (columnIndex) {
+      case 0:
+        isAscending == true
+          ? widget.county.locs.sort((loc1, loc2) => loc1.name.compareTo(loc2.name))
+          : widget.county.locs.sort((loc1, loc2) => loc2.name.compareTo(loc1.name));
+        break;
+      case 1:
+        isAscending == true
+          ? widget.county.locs.sort((loc1, loc2) => loc1.spots.compareTo(loc2.spots))
+          : widget.county.locs.sort((loc1, loc2) => loc2.spots.compareTo(loc1.spots));
+        break;
+      case 2:
+        isAscending == true
+          ? widget.county.locs.sort((loc1, loc2) => loc1.handicap.compareTo(loc2.handicap))
+          : widget.county.locs.sort((loc1, loc2) => loc2.handicap.compareTo(loc1.handicap));
+        break;
+    }
+    setState(() {
+      _columnIndex = columnIndex;
+      _isAscending = isAscending;
+    });
   }
 }
