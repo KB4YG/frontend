729c1f323dac82ee77ad2956ec88c0b780af8831
clemoss <christopher.lemoss@gmail.com>
2022-02-21 18:03:31 -0800

    Removed bottom padding to look better above listview

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -10,7 +10,7 @@ class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.fromLTRB(8,8,8,0),
       child: TextField(
         onChanged: onChanged,
         controller: editingController,
