acd65cbdb45e01e9f24c981b99cb3e6710b32c0a
clemoss <christopher.lemoss@gmail.com>
2022-03-09 20:48:50 -0800

    Condensed to single endpoint

--- lib/providers/backend.dart
+++ lib/providers/backend.dart
@@ -12,13 +12,13 @@ class BackendProvider {
   static const domain = 'cfb32cwake.execute-api.us-west-2.amazonaws.com';
   BackendProvider();
 
-  Future<http.Response> queryBackend(String path, Map<String, String>? parameters) async {
+  Future<http.Response> queryBackend(Map<String, String>? parameters) async {
     print('API CALL - parameters: $parameters');
-    var url = Uri.https(domain, path, parameters);
+    var url = Uri.https(domain, '/location', parameters);
     var response = await http.get(url, headers: {'Accept': 'application/json'});
     print(response.statusCode);
     // print(response.body);
-    if (response.statusCode == 200) {
+    if (response.statusCode   == 200) {
       return response;
     } else {
       throw Exception('Failed to fetch location data from API');
@@ -26,35 +26,35 @@ class BackendProvider {
   }
 
   Future<ParkingLot> getParkingLot(String parkingLotName) async {
-    var response = await queryBackend('/location', {'parkingLotName': parkingLotName});
+    var response = await queryBackend({'parkingLotName': parkingLotName});
     var jsonObj = json.decode(response.body);
     var parkingLot = ParkingLot.fromJson(jsonObj[0]);
     return parkingLot;
   }
 
   Future<RecreationArea> getRecreationArea(String recreationAreaUrl) async {
-    var response = await queryBackend('/location', {'ParkURL': recreationAreaUrl});
+    var response = await queryBackend({'parkURL': recreationAreaUrl});
     // check if not empty for RecreationArea/County or error code in future
-    var jsonObj = List<Map<String, dynamic>>.from(json.decode(response.body));
+    var jsonObj = Map<String, dynamic>.from(json.decode(response.body));
     var recreationArea = RecreationArea.fromJson(jsonObj);
     return recreationArea;
   }
 
   Future<County> getCounty(String countyName) async {
-    var response = await queryBackend('/location', {'county': countyName});
+    var response = await queryBackend({'county': countyName});
     var jsonObj = json.decode(response.body);
     var county = County.fromJson(jsonObj);
     return county;
   }
 
   Future<List<String>> getCountyList({String state = 'OR'}) async {
-    var response = await queryBackend('/location', {'county': 'all'});
+    var response = await queryBackend({'county': 'all'});
     var jsonObj = json.decode(response.body);
     var countyList = List<String>.from(jsonObj['Counties']);
     return countyList;
   }
 
-  // Context accessor ( var backend = BackendProvider.of(context); )
+  // Context accessor (ex: "var backend = BackendProvider.of(context);" )
   static BackendProvider of(BuildContext context) =>
       Provider.of<BackendProvider>(context, listen: false);
 }
