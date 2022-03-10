14083f05b8c2810da92735aaada6512357fd1b19
clemoss <christopher.lemoss@gmail.com>
2022-03-09 20:48:07 -0800

    Adapted to backend changes

--- lib/models/recreation_area.dart
+++ lib/models/recreation_area.dart
@@ -5,23 +5,18 @@ import 'fire_danger.dart';
 class RecreationArea {
   final String name;
   final String info;
-  final List<String> imageUrls = [
-    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-3.jpg'
-  ];
+  final List<String> imageUrls;
   final List<ParkingLot> parkingLots;
-  int get spots =>
+  late final int spots =
       [for (var x in parkingLots) x.spots].fold(0, (p, c) => p + c);
-  int get handicap =>
+  late final int handicap =
       [for (var x in parkingLots) x.handicap].fold(0, (p, c) => p + c);
   late final FireDanger fireDanger = [for (var x in parkingLots) x.fireDanger]
       .fold(parkingLots[0].fireDanger, (p, c) => p.level.index > c.level.index ? p : c);
 
-  RecreationArea(this.name, this.info, this.parkingLots);
-
-  RecreationArea.fromJson(List<Map<String, dynamic>> json)
-      : name = json[0]['RecreationArea'],
-        info = json[0]['About'],
-        parkingLots = [for (var item in json) ParkingLot.fromJson(item)];
+  RecreationArea.fromJson(Map<String, dynamic> json)
+      : name = json['RecreationArea'],
+        info = json['About'],
+        imageUrls = json['Images'],
+        parkingLots = [for (var item in json['List']) ParkingLot.fromJson(item)];
 }
