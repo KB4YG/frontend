4a2aa25a17bd681e528939697f4ba9fdded0c126
clemoss <christopher.lemoss@gmail.com>
2022-04-04 20:55:07 -0700

    Added hintText parameter

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 class SearchBar extends StatelessWidget {
   final TextEditingController editingController;
   final void Function(String)? onChanged;
+  final String? hintText;
   const SearchBar(
-      {Key? key, required this.editingController, required this.onChanged})
+      {Key? key,
+      required this.editingController,
+      required this.onChanged,
+      this.hintText = 'Search'})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(8,8,8,0),
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
       child: TextField(
         onChanged: onChanged,
         controller: editingController,
-        decoration: const InputDecoration(
-            hintText: 'Search',
-            prefixIcon: Icon(Icons.search),
-            border: OutlineInputBorder(
+        decoration: InputDecoration(
+            hintText: hintText,
+            prefixIcon: const Icon(Icons.search),
+            border: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(25.0)))),
       ),
     );
