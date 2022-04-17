24a71c70adf5e3e778d5a108f780f89c2d4aaee1
clemoss <christopher.lemoss@gmail.com>
2022-04-17 11:40:52 -0700

    Stylized themes

--- lib/providers/theme.dart
+++ lib/providers/theme.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart' show kDebugMode;
+import 'package:flutter/foundation.dart' show kDebugMode, kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:kb4yg/utilities/constants.dart' as constants;
 import 'package:shared_preferences/shared_preferences.dart'
@@ -43,16 +43,35 @@ class Themes {
   static final lightTheme = ThemeData(
       fontFamily: 'OpenSans',
       textTheme: textTheme,
-      scaffoldBackgroundColor: const Color(0xFFF6F7F9),
+      scaffoldBackgroundColor: const Color(0xfff6f7f9),
       primarySwatch: Colors.green,
+      cardColor: const Color(0xfffefefe),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            onPrimary: Colors.green.shade600,
+            primary: Colors.green.shade50,
+            side: BorderSide(color: Colors.green.shade100)),
+      ),
       colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green)
           .copyWith(tertiary: Colors.lightGreen));
 
   static final darkTheme = ThemeData(
       fontFamily: 'OpenSans',
       textTheme: textTheme,
+      indicatorColor: Colors.lightBlue.shade300,
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Colors.blueGrey, foregroundColor: Colors.white),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            onPrimary: Colors.blueGrey.shade50,
+            primary: Colors.blueGrey.shade700,
+            side: BorderSide(color: Colors.blueGrey.shade600)),
+      ),
       colorScheme: ColorScheme.fromSwatch(
-              brightness: Brightness.dark, primarySwatch: Colors.blue)
+              brightness: Brightness.dark,
+              primarySwatch: kIsWeb ? Colors.blueGrey : Colors.lightBlue)
           .copyWith(
-              tertiary: Colors.grey.shade800, secondary: Colors.lightBlue));
+        tertiary: Colors.blueGrey.shade900,
+        secondary: Colors.lightBlue.shade300,
+      ));
 }
