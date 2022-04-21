3908381fa8976235d508d44a4c45160254743c3c
clemoss <christopher.lemoss@gmail.com>
2022-04-20 20:55:55 -0700

    Wrapped in card

--- lib/widgets/error_card.dart
+++ lib/widgets/error_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kb4yg/widgets/expanded_section.dart';
 
@@ -24,51 +25,51 @@ class _ErrorCardState extends State<ErrorCard> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Flexible(
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 600),
-              child: ListTile(
-                  dense: false,
-                  visualDensity: VisualDensity.comfortable,
-                  leading: const SizedBox(
-                    height: double.infinity,
-                    child: Icon(Icons.error, color: Colors.red, size: 30),
-                  ),
-                  title: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: Text(widget.title),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(widget.body),
-                      if (widget.message != null)
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: 600),
+        child: Card(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              dense: false,
+              visualDensity: VisualDensity.comfortable,
+              leading: const SizedBox(
+                height: double.infinity,
+                child: Icon(Icons.error, color: Colors.red, size: 30),
+              ),
+              title: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(widget.title),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(widget.body),
+                  if (widget.message != null)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
                         TextButton(
                             onPressed: () =>
                                 setState(() => _viewMessage = !_viewMessage),
                             child: Text(
-                              '${_viewMessage ? 'Hide' : 'View'} Details',
-                              textAlign: TextAlign.left,
-                            )),
-                      if (widget.message != null)
+                                '${_viewMessage ? 'Hide' : 'View'} Details')),
                         Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
                           child: ExpandedSection(
-                              expand: _viewMessage,
-                              child:
-                                  Text('Error Message: "${widget.message}"')),
+                            expand: _viewMessage,
+                            child: SelectableText(widget.message!),
+                          ),
                         ),
-                    ],
-                  )),
+                      ],
+                    )
+                ],
+              ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
