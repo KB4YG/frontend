185e950e0da84edd533a0f6b11c15f22563d03cc
clemoss <christopher.lemoss@gmail.com>
2022-04-20 20:53:22 -0700

    Wrapped in card

--- lib/screens/not_found_screen.dart
+++ lib/screens/not_found_screen.dart
@@ -1,6 +1,7 @@
 import 'package:beamer/beamer.dart' show BeamPage, BeamPageType, Beamer;
 import 'package:flutter/material.dart';
 import 'package:kb4yg/utilities/constants.dart' as constants;
+import 'package:kb4yg/widgets/content_card.dart';
 import 'package:kb4yg/widgets/screen_template.dart';
 
 class NotFoundScreen extends StatelessWidget {
@@ -16,36 +17,37 @@ class NotFoundScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenTemplate(
         title: const Text(constants.title),
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 30.0),
-          constraints: const BoxConstraints(maxWidth: 1000),
-          child: Column(
-            children: [
-              Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 40),
-                  child: SelectableText.rich(
-                    TextSpan(children: [
-                      TextSpan(
-                          text: 'Page Not Found\n\n',
-                          style: Theme.of(context).textTheme.headline4),
-                      const TextSpan(
-                          text:
-                              "Uh oh, we can't seem to find the page you're looking for.\n\n"
-                              "Try navigating to the previous page or use the button below to go to the home screen."),
-                    ], style: Theme.of(context).textTheme.bodyText1),
-                  )),
-              Padding(
-                padding: const EdgeInsets.only(top: 30.0),
-                child: ElevatedButton(
-                    onPressed: () =>
-                        Beamer.of(context).beamToNamed(constants.routeHome),
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
-                      child: Text('Return Home', textScaleFactor: 1.1),
+        child: ScreenCard(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30.0),
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: SelectableText.rich(
+                      TextSpan(children: [
+                        TextSpan(
+                            text: 'Page Not Found\n\n',
+                            style: Theme.of(context).textTheme.headline4),
+                        const TextSpan(
+                            text:
+                                "Uh oh, we can't seem to find the page you're looking for.\n\n"
+                                "Try navigating to the previous page or use the button below to go to the home screen."),
+                      ], style: Theme.of(context).textTheme.bodyText1),
                     )),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: ElevatedButton(
+                      onPressed: () =>
+                          Beamer.of(context).beamToNamed(constants.routeHome),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
+                        child: Text('Return Home', textScaleFactor: 1.1),
+                      )),
+                )
+              ],
+            ),
           ),
         ));
   }
