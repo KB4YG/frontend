10125190405ff914ab7c43d31508c076a68ac4b4
clemoss <christopher.lemoss@gmail.com>
2022-04-22 17:10:21 -0700

    Updated launch url

--- lib/utilities/launch_url.dart
+++ lib/utilities/launch_url.dart
@@ -1,5 +1,5 @@
 import 'package:url_launcher/url_launcher.dart';
 
 void launchURL(String url) async {
-  if (!await launch(url)) throw 'Could not launch $url';
+  if (!await launchUrl(Uri.parse(url))) throw 'Could not launch $url';
 }
