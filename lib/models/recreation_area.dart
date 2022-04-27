98f1c806882542bae644299b15e9a4bd03675d61
clemoss <christopher.lemoss@gmail.com>
2022-04-26 19:47:10 -0700

    Made timestamp least recent

--- lib/models/recreation_area.dart
+++ lib/models/recreation_area.dart
@@ -4,15 +4,20 @@ import 'fire_danger.dart';
 
 class RecreationArea {
   final String name;
-  final String info;
-  final String parkingLotUrl;
-  final List<String> imageUrls;
+  final String info; // Description of recreation area
+  final String parkingLotUrl; // URL path to parking lot
+  final List<String> imageUrls; // Web address of associated nature images
   final List<ParkingLot> parkingLots;
-  late final DateTime dt = parkingLots[0].dt;
+  // Sum general spots
   late final int spots =
       [for (var x in parkingLots) x.spots].fold(0, (p, c) => p + c);
+  // Sum handicap spots
   late final int handicap =
       [for (var x in parkingLots) x.handicap].fold(0, (p, c) => p + c);
+  // Get least recent timestamp amongst parking lots
+  late final DateTime dt = [for (var x in parkingLots) x.dt]
+      .fold(parkingLots[0].dt, (p, c) => p.isAfter(c) ? c : p);
+  // Get highest fire danger level enum amongst parking lots
   late final FireDanger fireDanger = [for (var x in parkingLots) x.fireDanger]
       .fold(parkingLots[0].fireDanger,
           (p, c) => p.level.index > c.level.index ? p : c);
@@ -25,6 +30,7 @@ class RecreationArea {
         parkingLots = [
           for (var item in json['List']) ParkingLot.fromJson(item)
         ] {
+    // Alphabetize parking lots by name
     parkingLots.sort((loc1, loc2) => loc1.name.compareTo(loc2.name));
   }
 
