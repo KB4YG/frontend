b8cd3aaeb2f23da92db7e24dfe6f674af5823622
clemoss <christopher.lemoss@gmail.com>
2022-05-25 15:59:24 -0700

    Merged Danmar's introduction screen changes

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -6,10 +6,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../widgets/app.dart';
 
-class IntroScreen extends StatelessWidget {
+class IntroScreen extends StatefulWidget {
   const IntroScreen({Key? key}) : super(key: key);
 
-  void _onIntroEnd(BuildContext context) async {
+  static const PageDecoration decoration = PageDecoration(
+    titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+    bodyTextStyle: TextStyle(fontSize: 19.0),
+    bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+    pageColor: Colors.white,
+    imagePadding: EdgeInsets.zero,
+  );
+
+  static const TextStyle buttonStyle =
+      TextStyle(fontWeight: FontWeight.w600, color: Colors.white);
+
+  @override
+  State<IntroScreen> createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  int _index = 0;
+
+  void onIntroEnd(BuildContext context) async {
     var prefs = Provider.of<SharedPreferences>(context, listen: false);
     await prefs.setBool(constants.prefIntro, false);
     Navigator.pushReplacement(
@@ -22,60 +40,44 @@ class IntroScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: IntroductionScreen(
+        onChange: (index) => setState(() => _index = index),
         pages: [
           PageViewModel(
-            title: "Testing Screen 1",
-            body:
-                "Many parameters are available, in next section of example all are not listed. To see all parameters available please check end of README.",
-            image: Image.asset('assets/launcher/ios_icon.png', width: 200),
-            decoration: const PageDecoration(
-              titleTextStyle:
-                  TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
-              bodyTextStyle: TextStyle(fontSize: 19.0),
-              bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-              pageColor: Colors.green,
-              imagePadding: EdgeInsets.zero,
-            ),
+            title: 'Check Parking Availability',
+            body: 'Before traveling to a recreation area, check parking '
+                'availability to ensure that you find a parking space upon arrival.',
+            image: Image.asset('assets/intro/intro-1.png', width: 230),
+            decoration: IntroScreen.decoration,
           ),
           PageViewModel(
-            title: "Testing Screen 2",
-            body:
-                "Introduction screen allow you to have a screen at launcher for example, where you can explain your app. ",
-            image: Image.asset('assets/launcher/kb4yg.png', width: 200),
-            decoration: const PageDecoration(
-              titleTextStyle:
-                  TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
-              bodyTextStyle: TextStyle(fontSize: 19.0),
-              bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-              pageColor: Colors.green,
-              imagePadding: EdgeInsets.zero,
-            ),
+            title: 'Be Aware of Fire Danger',
+            body: 'View the likelihood of fire ignition for a recreation area '
+                'to make informed decisions and stay safe.',
+            image: Image.asset('assets/intro/intro-2.png', width: 230),
+            decoration: IntroScreen.decoration,
           ),
           PageViewModel(
-            title: "Testing Screen 3",
-            body:
-                "Introduction screen allow you to have a screen at launcher for example, where you can explain your app. ",
-            image: Image.asset('assets/launcher/android_icon.png', width: 200),
-            decoration: const PageDecoration(
-              titleTextStyle:
-                  TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
-              bodyTextStyle: TextStyle(fontSize: 19.0),
-              bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-              pageColor: Colors.green,
-              imagePadding: EdgeInsets.zero,
-            ),
-          )
+            title: 'Scope Out Beautiful Landscapes',
+            body: 'View images of recreation areas to help decide where to go.',
+            image: Image.asset('assets/intro/intro-3.png', width: 230),
+            decoration: IntroScreen.decoration,
+          ),
+          PageViewModel(
+            title: 'View Maps and Directions',
+            body: 'Locate recreation areas near you using our map, and '
+                'get directions to natural sites with the click of a button!',
+            image: Image.asset('assets/intro/intro-4.png', width: 230),
+            decoration: IntroScreen.decoration,
+          ),
         ],
-        onDone: () => _onIntroEnd(context),
-        showBackButton: false,
-        showSkipButton: true,
-        skip: const Text('Skip',
-            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
-        next: const Text('Next',
-            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
+        onDone: () => onIntroEnd(context),
+        showBackButton: _index != 0,
+        showSkipButton: _index == 0,
+        skip: const Text('Skip', style: IntroScreen.buttonStyle),
+        back: const Text('Back', style: IntroScreen.buttonStyle),
+        next: const Text('Next', style: IntroScreen.buttonStyle),
+        done: const Text('Done', style: IntroScreen.buttonStyle),
         globalBackgroundColor: Colors.green,
-        done: const Text("Done",
-            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
         dotsDecorator: DotsDecorator(
             size: const Size.square(.0),
             activeSize: const Size(50.0, 10.0),
